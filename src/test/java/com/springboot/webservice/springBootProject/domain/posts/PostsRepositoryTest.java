d3fc6cd747e450a46e61d09861afd5ca9cbe7011
sojeonglee <sojeonglee0125@gmail.com>
2022-10-22 17:04:24 +0900

    test : BaseTimeEntity로 객체 생성 수정 시간 기록기능 확인 테스트작성

--- src/test/java/com/springboot/webservice/springBootProject/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/springboot/webservice/springBootProject/domain/posts/PostsRepositoryTest.java
@@ -1,15 +1,13 @@
 package com.springboot.webservice.springBootProject.domain.posts;
 
-import org.aspectj.lang.annotation.After;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PostsRepositoryTest {
@@ -41,5 +39,27 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         Assertions.assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_regis(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 10, 22, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>> createdDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        Assertions.assertThat(posts.getCreatedDate()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
