c6fcc9d9f9057bb7f350c9bb34ef7e96cfc4a922
sojeonglee <sojeonglee0125@gmail.com>
2022-10-22 14:33:05 +0900

    test : 기본 컨트롤러 get 요청 테스트코드 작성

--- src/test/java/com/springboot/webservice/springBootProject/web/HelloControllerTest.java
+++ src/test/java/com/springboot/webservice/springBootProject/web/HelloControllerTest.java
@@ -3,7 +3,6 @@ package com.springboot.webservice.springBootProject.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
