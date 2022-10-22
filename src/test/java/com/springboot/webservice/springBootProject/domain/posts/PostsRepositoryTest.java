3114cb7cd11f901dc146d54dcf7e9c8f4088228f
sojeonglee <sojeonglee0125@gmail.com>
2022-10-22 15:14:34 +0900

    test : posts 저장 테스트 코드 작성

---
+++ src/test/java/com/springboot/webservice/springBootProject/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.springboot.webservice.springBootProject.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void post_save_call() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("sojeongLee")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
