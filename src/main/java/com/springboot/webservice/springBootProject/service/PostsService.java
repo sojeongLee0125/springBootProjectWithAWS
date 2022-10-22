0c72e089c551c70f170b3c7297fe91e0ce67261a
sojeonglee <sojeonglee0125@gmail.com>
2022-10-22 15:45:56 +0900

    feat : POSTS 게시글 Sevice 클래스 생성

---
+++ src/main/java/com/springboot/webservice/springBootProject/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.springboot.webservice.springBootProject.service;
+
+import com.springboot.webservice.springBootProject.domain.posts.PostsRepository;
+import com.springboot.webservice.springBootProject.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
