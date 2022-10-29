7d6ccdf08ef535f20663ef20eebc36d2e89b1454
sojeonglee <sojeonglee0125@gmail.com>
2022-10-29 17:06:09 +0900

    feat : 게시글 업데이크 화면 및 컨트롤러 매핑 추가

--- src/main/java/com/springboot/webservice/springBootProject/web/IndexController.java
+++ src/main/java/com/springboot/webservice/springBootProject/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.springboot.webservice.springBootProject.web;
 
 import com.springboot.webservice.springBootProject.service.PostsService;
+import com.springboot.webservice.springBootProject.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +22,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
