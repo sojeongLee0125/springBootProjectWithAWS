f99eba92d06f46d50d144bd699473cba26013672
sojeonglee <sojeonglee0125@gmail.com>
2022-10-30 14:40:14 +0900

    fix : git cache 정리

---
+++ src/main/java/com/springboot/webservice/springBootProject/SpringBootProjectApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.webservice.springBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
