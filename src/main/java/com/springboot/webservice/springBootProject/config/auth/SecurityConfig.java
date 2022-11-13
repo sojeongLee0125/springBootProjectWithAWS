99e07f73f17e28dc7a1391690ca01210c2175726
sojeonglee <sojeonglee0125@gmail.com>
2022-11-13 15:06:19 +0900

    feat : 글쓰기 권한 수정

--- src/main/java/com/springboot/webservice/springBootProject/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot/webservice/springBootProject/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**")
-                .hasRole(Role.USER.name())
+                .hasRole(Role.GUEST.name())
                 .anyRequest().authenticated()
                 .and()
                 .logout()
